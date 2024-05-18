84199b7d679659dba43dce4fd563d60e6f1528ce
BensonGitau <bensonkamaugitau@gmail.com>
2024-05-18 10:06:11 +0300

    useRef

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,11 +2,15 @@ import React, { useRef } from "react";
 
 function Box() {
   const elementRef = useRef();
+  function handleMesureClick(){
+    const div = elementRef.current;
+    console.log("measurements:", div.getBoundingClientRect());
+  }
 
   return (
     <div ref={elementRef}>
       <h1>Box</h1>
-      <button>Measure</button>
+      <button onClick={handleMesureClick}>Measure</button>
     </div>
   );
 }
